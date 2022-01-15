5c2aa6c45e0fceaaa00d27ef15833945cf867f62
Steven Fernández <vsteven25@gmail.com>
2022-01-14 19:39:28 -0600

    Add token validation on views
    Defined the default permission as IsAuthenticated
    since, at the moment, the login view is the only
    one that does not require an authenticated user or
    token.

--- sellement/sellement/settings.py
+++ sellement/sellement/settings.py
@@ -132,3 +132,12 @@ AUTH_USER_MODEL = 'users.User'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
--- sellement/users/views.py
+++ sellement/users/views.py
@@ -1,6 +1,9 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+from django.contrib.auth import logout as django_logout
 
 from users.serializers import UserLoginSerializer
 from users.serializers import UserModelSerializer
@@ -17,13 +20,27 @@ class UserViewSet(viewsets.GenericViewSet):
 
         return UserModelSerializer
 
-    @action(detail=False, methods=['POST'])
+    @action(detail=False, methods=['POST'],
+            permission_classes=[AllowAny])
     def login(self, request):
         serializer = UserLoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user, token = serializer.save()
-        response = {
-            'user': UserModelSerializer(user).data,
-            'access_token': token
-        }
-        return Response(response, status=status.HTTP_201_CREATED)
+        response = {}
+        status_code = 0
+
+        if not serializer.is_valid():
+            response['error'] = 'Invalid credentials'
+            status_code = status.HTTP_400_BAD_REQUEST
+        else:
+            user, token = serializer.save()
+            response['user'] = UserModelSerializer(user).data
+            response['access_token'] = token
+            status_code = status.HTTP_201_CREATED
+
+        return Response(response, status=status_code)
+
+    @action(detail=False, methods=['GET'])
+    def logout(self, request):
+        request.user.auth_token.delete()
+        django_logout(request)
+
+        return Response(status=status.HTTP_200_OK)
